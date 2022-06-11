dd19dc5194f046c0e98724ad40185a30f9210a76
Ashenguard <myminecraft2019@gmail.com>
2022-06-11 18:26:27 +0430

    4.0.1 - MessageMode documented

--- src/main/java/me/ashenguard/api/messenger/MessageMode.java
+++ src/main/java/me/ashenguard/api/messenger/MessageMode.java
@@ -8,16 +8,45 @@ import java.util.Collections;
 
 @SuppressWarnings({"SameParameterValue", "unused"})
 public enum MessageMode {
+    /**
+     * Info message, Sent to console and people with info perm; Usually contains important messages.
+     */
     Info(ChatColor.GREEN),
+    /**
+     * Warning message, Sent to console and people with warning perm; Usually contains important messages about issues.
+     */
     Warning(ChatColor.RED),
+    /**
+     * Critical message, Sent to console and people with warning perm; Same as warning, But it will be always sent to at least one operator, If none is online, It will be kept till one login
+     */
     Critical(ChatColor.DARK_RED, "Warning"),
+    /**
+     * Debug message, Sent to console and people with debug perm; Useless messages just for debugging.
+     */
     Debug(ChatColor.YELLOW),
+    /**
+     * Operator message, Sent to console and only operators;
+     */
     Operator(ChatColor.AQUA, "*"),
-    Personal(ChatColor.LIGHT_PURPLE, null, ""),
+    /**
+     * Personal message, Sent to a target; It will have the plugin prefix
+     */
+    Personal(ChatColor.LIGHT_PURPLE, null, "", false),
+    /**
+     * Broadcast message, Sent to all online players; It will have no prefix
+     */
     BroadCast(ChatColor.DARK_RED, null, null),
-    Empty(ChatColor.RESET, null, null);
+    /**
+     * Critical broadcast message, Sent to all online players; It will have no prefix; If a player is offline the message will be kept for them until they login;
+     */
+    CriticalBroadCast(ChatColor.DARK_RED, null, null),
+    /**
+     * Simple message, Sent to a target; It won't have any prefix
+     */
+    Empty(ChatColor.RESET, null, null, false);
 
     public final String permission;
+    public final boolean sendAll;
     private final ChatColor color;
     private final String prefix;
 
@@ -28,6 +57,9 @@ public enum MessageMode {
         this(color, permission, "NAME");
     }
     MessageMode(ChatColor color, String permission, String prefix) {
+        this(color, permission, prefix, true);
+    }
+    MessageMode(ChatColor color, String permission, String prefix, boolean sendAll) {
         this.color = color;
 
         if (permission == null) this.permission = null;
@@ -37,6 +69,8 @@ public enum MessageMode {
 
         if (prefix != null && prefix.equals("NAME")) this.prefix = name();
         else this.prefix = prefix;
+
+        this.sendAll = sendAll;
     }
 
     public boolean hasPermission(SpigotPlugin plugin, Permissible permissible) {
