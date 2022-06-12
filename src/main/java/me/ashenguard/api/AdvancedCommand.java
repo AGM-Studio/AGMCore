006b73a32eea82da418d64919e0190503deaff3b
Ashenguard <myminecraft2019@gmail.com>
2022-06-12 12:27:37 +0430

    4.0.1 - AdvancedCommand Assertion

--- src/main/java/me/ashenguard/api/AdvancedCommand.java
+++ src/main/java/me/ashenguard/api/AdvancedCommand.java
@@ -1,6 +1,7 @@
 package me.ashenguard.api;
 
 import me.ashenguard.api.spigot.SpigotPlugin;
+import me.ashenguard.exceptions.InstanceAssertionError;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,44 +11,37 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.logging.Level;
 
 @SuppressWarnings({"SameReturnValue", "unused", "ConstantConditions"})
 public abstract class AdvancedCommand implements CommandExecutor, TabCompleter {
     protected final SpigotPlugin plugin;
     protected final String name;
-    protected final boolean playerOnly;
-    protected final String playerOnlyMessage;
 
-    public AdvancedCommand(SpigotPlugin plugin, String name, boolean playerOnly) {
-        this(plugin, name, playerOnly, "§cError:§r This command can be executed by a player");
-    }
+    protected Predicate<String[]> playerRequired = (args) -> false;
 
-    public AdvancedCommand(SpigotPlugin plugin, String name, boolean playerOnly, String playerOnlyMessage) {
+    public AdvancedCommand(SpigotPlugin plugin, String name) {
         plugin.getCommand(name).setExecutor(this);
         plugin.getCommand(name).setTabCompleter(this);
 
         this.plugin = plugin;
         this.name = name;
-        this.playerOnly = playerOnly;
-        this.playerOnlyMessage = playerOnlyMessage;
     }
 
     public abstract void run(CommandSender sender, Command command, String label, String[] args);
     public abstract List<String> tabs(CommandSender sender, Command command, String alias, String[] args);
-    protected boolean playerOnlyCondition(CommandSender sender, Command command, String label, String[] args) {
-        return true;
-    }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         try {
             if (command.getName().equalsIgnoreCase(name)) {
-                if (playerOnly && playerOnlyCondition(sender, command, label, args))
-                    if (sender instanceof Player) run(sender, command, label, args);
-                    else plugin.messenger.response(sender, "§cError:§r This command can be executed by a player");
-                else run(sender, command, label, args);
+                if (playerRequired.test(args)) InstanceAssertionError.check(sender, Player.class, plugin.translation.get("PlayerOnly", null));
+                run(sender, command, label, args);
             }
+        } catch (AssertionError assertion) {
+            if (assertion.getMessage() != null && assertion.getMessage().length() > 0)
+                plugin.messenger.response(sender, assertion.getMessage().split("\n"));
         } catch (Throwable throwable) {
             plugin.getLogger().log(Level.WARNING, String.format("Command %s for %s generated an exception", this.name, plugin.getDescription().getFullName()), throwable);
             plugin.messenger.response(sender, "§cWhile handling your request an unexpected error happened...§r");
